06351ff77141bb6829bfafe86f585005cbd4f55e
Matthew Toghill <matthew.toghill@outlook.com>
2023-12-03 12:43:12 +0000

    refactor day 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -3,53 +3,49 @@
 public class Program
 {
     private static readonly string[] _input = Input.ReadAllLines();
+    private static readonly (int red, int green, int blue) _bagMax = (12, 13, 14);
     static void Main()
     {
-        var (part1, part2) = BothParts(12, 13, 14);
+        var (part1, part2) = BothParts();
         Console.WriteLine($"Part 1: {part1}");
         Console.WriteLine($"Part 2: {part2}");
     }
 
-    private static (int part1, int part2) BothParts(int red, int green, int blue)
+    private static (int part1, int part2) BothParts()
     {
         var validGames = new List<int>();
         var gamePowers = new List<int>();
 
         foreach (var line in _input)
         {
-            var gameId = int.Parse(line.SplitOn(StringSplitOptions.RemoveEmptyEntries, "Game ", ":")[0]);
-            var rounds = line.SplitOn(StringSplitOptions.RemoveEmptyEntries, "Game", ":", ";").Skip(1).ToArray();
+            var gameId = int.Parse(line.Split(["Game ", ":"], StringSplitOptions.RemoveEmptyEntries)[0]);
+            var cubes = line.Split(["Game", ":", ";", ",", " "], StringSplitOptions.RemoveEmptyEntries).Skip(1).Chunk(2).ToArray();
             bool isImpossible = false;
-            int redMin = 0, greenMin = 0, blueMin = 0;
+            int redMax = 0, greenMax = 0, blueMax = 0;
 
-            foreach (var round in rounds)
+            foreach(var cube in cubes)
             {
-                var cubes = round.SplitOn(StringSplitOptions.RemoveEmptyEntries, ",", " ").Chunk(2).ToArray();
+                var num = int.Parse(cube[0]);
+                var colour = cube[1];
 
-                foreach(var cube in cubes)
+                if (colour == "red")
                 {
-                    var num = int.Parse(cube[0]);
-                    var colour = cube[1];
-
-                    if (colour == "red")
-                    {
-                        if (num > red) isImpossible = true;
-                        if (num > redMin) redMin = num;
-                    }
-                    else if (colour == "green")
-                    {
-                        if (num > green) isImpossible = true;
-                        if (num > greenMin) greenMin = num;
-                    }
-                    else if (colour == "blue")
-                    {
-                        if (num > blue) isImpossible = true;
-                        if (num > blueMin) blueMin = num;
-                    }
+                    if (num > _bagMax.red) isImpossible = true;
+                    redMax = Math.Max(redMax, num);
+                }
+                else if (colour == "green")
+                {
+                    if (num > _bagMax.green) isImpossible = true;
+                    greenMax = Math.Max(greenMax, num);
+                }
+                else if (colour == "blue")
+                {
+                    if (num > _bagMax.blue) isImpossible = true;
+                    blueMax = Math.Max(blueMax, num);
                 }
             }
 
-            gamePowers.Add(redMin * greenMin * blueMin);
+            gamePowers.Add(redMax * greenMax * blueMax);
             if (!isImpossible) validGames.Add(gameId);
         }
 
