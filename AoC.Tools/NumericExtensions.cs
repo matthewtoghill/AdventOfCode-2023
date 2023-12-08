62b76455471344401583540a1db8e91c17c0658c
Matthew Toghill <matthew.toghill@outlook.com>
2023-12-08 09:31:10 +0000

    Add day 8

--- AoC.Tools/NumericExtensions.cs
+++ AoC.Tools/NumericExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC.Tools;
+﻿using System.Numerics;
+
+namespace AoC.Tools;
 
 public static class NumericExtensions
 {
@@ -32,5 +34,56 @@ public static class NumericExtensions
     {
         return minPos.X > pos.X || pos.X > maxPos.X
             || minPos.Y > pos.Y || pos.Y > maxPos.Y;
+    }
+
+
+    public static long GreatestCommonFactor(int x, int y)
+    {
+        var b = Math.Max(x, y);
+        var r = Math.Min(x, y);
+
+        while (r != 0)
+        {
+            var a = b;
+            b = r;
+            r = a % b;
+        }
+
+        return b;
+    }
+
+    public static long GreatestCommonFactor(long x, long y)
+    {
+        var b = Math.Max(x, y);
+        var r = Math.Min(x, y);
+
+        while (r != 0)
+        {
+            var a = b;
+            b = r;
+            r = a % b;
+        }
+
+        return b;
+    }
+
+    public static long LowestCommonMultiple(int x, int y) => x / GreatestCommonFactor(x, y) * y;
+    public static long LowestCommonMultiple(long x, long y) => x / GreatestCommonFactor(x, y) * y;
+
+    public static long LowestCommonMultiple(this IEnumerable<int> items)
+    {
+        var uniqueValues = new HashSet<int>(items).ToArray();
+
+        if (uniqueValues.Length == 0) return 0;
+        if (uniqueValues.Length == 1) return uniqueValues[0];
+
+        var current = LowestCommonMultiple(uniqueValues[0], uniqueValues[1]);
+
+        for (int i = 2; i < uniqueValues.Length; i++)
+        {
+            current = LowestCommonMultiple(current, uniqueValues[i]);
+        }
+
+        return current;
     }
 }
---
+++ Day08/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Day08;
+
+public class Program
+{
+    private static readonly string[] _input = Input.ReadAllLines();
+    static void Main()
+    {
+        Console.WriteLine($"Part 1: {Part1()}");
+        Console.WriteLine($"Part 2: {Part2()}");
+    }
+
+    private static int Part1()
+    {
+        var (instructions, nodes) = ParseInput();
+
+        var current = "AAA";
+        var steps = 0;
+
+        while (current != "ZZZ")
+        {
+            var node = nodes[current];
+            switch (instructions[steps % instructions.Length])
+            {
+                case 'L': current = node.Left; break;
+                case 'R': current = node.Right; break;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+
+    private static long Part2()
+    {
+        var (instructions, nodes) = ParseInput();
+        var startingNodes = nodes.Keys.Where(x => x.EndsWith('A')).ToArray();
+        var stepList = new List<int>();
+
+        foreach (var start in startingNodes)
+        {
+            var current = start;
+            var steps = 0;
+
+            while (!current.EndsWith('Z'))
+            {
+                var node = nodes[current];
+                switch (instructions[steps % instructions.Length])
+                {
+                    case 'L': current = node.Left; break;
+                    case 'R': current = node.Right; break;
+                }
+
+                steps++;
+            }
+
+            stepList.Add(steps);
+        }
+
+        return stepList.LowestCommonMultiple();
+    }
+
+    private static (string, Dictionary<string, Node>) ParseInput()
+    {
+        Dictionary<string, Node> nodes = [];
+        foreach (var item in _input.Skip(2))
+        {
+            var split = item.Split(["=", "(", ",", ")", " "], StringSplitOptions.RemoveEmptyEntries);
+            var start = split[0];
+            nodes.Add(split[0], new Node(split[1], split[2]));
+        }
+
+        return (_input[0], nodes);
+    }
+}
+
+public record Node(string Left, string Right);
