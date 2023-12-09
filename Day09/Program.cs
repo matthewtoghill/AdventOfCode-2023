6516ca9c51b299958c271df1f9d874bbd3e2d09a
Matthew Toghill <matthew.toghill@outlook.com>
2023-12-09 09:29:42 +0000

    Add day 9

---
+++ Day09/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Day09;
+
+public class Program
+{
+    private static readonly List<List<int>> _input = Input.ReadAllLines().Select(x => x.ExtractNumeric<int>().ToList()).ToList();
+    static void Main()
+    {
+        var (part1, part2) = Solve();
+        Console.WriteLine($"Part 1: {part1}");
+        Console.WriteLine($"Part 2: {part2}");
+    }
+
+    private static (int, int) Solve()
+    {
+        List<int> firstNumbers = [];
+        List<int> lastNumbers = [];
+        foreach (var line in _input)
+        {
+            List<List<int>> gapSequences = [];
+            gapSequences.Add(line);
+            var current = line;
+            while (true)
+            {
+                List<int> differences = [];
+                for (int i = 1; i < current.Count; i++)
+                {
+                    differences.Add(current[i] - current[i - 1]);
+                }
+
+                gapSequences.Add(differences);
+                if (differences.All(x => x == 0)) break;
+                current = differences;
+            }
+
+            for (int i = gapSequences.Count - 2; i >= 0; i--)
+            {
+                gapSequences[i].Add(gapSequences[i][^1] + gapSequences[i + 1][^1]);
+                gapSequences[i].Insert(0, gapSequences[i][0] - gapSequences[i + 1][0]);
+            }
+
+            firstNumbers.Add(gapSequences[0][0]);
+            lastNumbers.Add(gapSequences[0][^1]);
+        }
+
+        return (lastNumbers.Sum(), firstNumbers.Sum());
+    }
+}
